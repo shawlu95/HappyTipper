42ea5f6299868e38ce36fa25e31b42ffb33693f3
Shaw Lu <shawlu95@gmail.com>
2023-12-07 22:59:39 -0800

    use textfield to enter bill amount

--- HappyTipper/ContentView.swift
+++ HappyTipper/ContentView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var bill: String = ""
     var body: some View {
-        VStack {
+        VStack(spacing: 30) {
+            Text("HappyTipper")
+                .font(.title2)
+                
             PriceCardView()
+            
+            VStack(alignment: .leading, content: {
+                Text("Enter your total bill amount:")
+                TextField("", text: $bill) // binding required
+                    .font(.system(size: 22))
+                    .padding(20) // text ditance to the left
+                    .frame(width: 350, height: 50)
+                    .background(.gray.opacity(0.2))
+                    .cornerRadius(12)
+            })
+            
         }
         .padding()
     }
