cdbd455a07355069efaa4b1de56b104bc0ea9600
Shaw Lu <shawlu95@gmail.com>
2023-12-07 22:17:37 -0800

    Initial Commit

---
+++ HappyTipper/HappyTipperApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyTipperApp.swift
+//  HappyTipper
+//
+//  Created by main on 2023/12/7.
+//
+
+import SwiftUI
+
+@main
+struct HappyTipperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
