de0d0607c5fdec04ad690cdf431034aed9b74327
Shaw Lu <shawlu95@gmail.com>
2023-12-07 23:05:35 -0800

    arrange v stack layout

--- HappyTipper/ContentView.swift
+++ HappyTipper/ContentView.swift
@@ -26,6 +26,10 @@ struct ContentView: View {
                     .background(.gray.opacity(0.2))
                     .cornerRadius(12)
                 
+            })
+            
+            // spacing 30 should apply to this one too
+            VStack() {
                 Text("Select your desired tip percentage:")
                 Picker("Tip", selection: $selectedPercent) {
                     Text("0%").tag(0)
@@ -33,9 +37,10 @@ struct ContentView: View {
                     Text("10%").tag(10)
                     Text("15%").tag(15)
                 }.pickerStyle(.segmented)
-            })
+            }
         }
-        .padding()
+        .padding(.leading, 20)
+        .padding(.trailing, 20)
     }
 }
 
